74c8543b11cccd8b315060133d550f11edb58c60
Tensor <narutokelechi@gmail.com>
2024-08-05 15:12:07 +0100

    [modify] add retrieval of audio metadata

--- music_data/config.py
+++ music_data/config.py
@@ -1,11 +1,19 @@
-class config:
+class scraper_config:
     sdd_dataset_url = "https://zenodo.org/records/10072001/files/audio.zip?download=1"
     num_files_target = 1000
+    outpath = "music"
+    num_pages = 10
 
     # freesound scraping configs
     freesound_url = "https://freesound.org/"
+    freesound_url_for_music = (
+        "https://freesound.org/browse/tags/?f=tag%3A%22music%22&page=1#sound"
+    )
     player_id = "bw-player"
     mp3_attr = "data-mp3"
+    duration_attr = "data-duration"
     container_class = "bw-search__result"
     mp3link_selector = "audio > source"
-    title_selector = "div.between > div > h5 > a"
+    title_selector = "h5.v-spacing-1 > a.bw-link--black"
+    tag_selector = "a.tag-container"
+    pagination_links = "bw-pagination_container"
--- music_data/music_scraper.py
+++ music_data/music_scraper.py
@@ -6,23 +6,46 @@ from .config import scraper_config
 from tqdm.auto import tqdm
 
 
-class MusicScraper:
+class FreeSoundMusicScraper:
     def __init__(self, config=scraper_config):
         self.site_link = config.freesound_url_for_music
         self.driver = init_driver(self.site_link)
         self.config = config
+        self.page_links = [
+            f"https://freesound.org/browse/tags/?f=tag%3A%22music%22&page={page_num}#sound"
+            for page_num in range(self.config.num_pages)
+        ]
+        self.filelinks = []
 
-    def link_collector(self):
-        section_links = self.driver.find_elements(
-            By.CSS_SELECTOR, self.config.player_id
-        )
+    def get_links(self, driver):
+        section_links = driver.find_elements(By.CSS_SELECTOR, self.config.player_id)
 
         audio_links = [
             x.get_attribute(self.config.mp3_attr) for x in tqdm(section_links)
         ]
 
+        durations = [
+            x.get_attribute(self.config.duration_attr) for x in tqdm(section_links)
+        ]
+
         print(f"got {len(audio_links)} links")
-        return audio_links
+
+        return {"audio_links": audio_links, "duration": durations}
+
+    def get_title(self, driver):
+        title_elements = driver.find_elements(
+            By.CSS_SELECTOR, self.config.title_selector
+        )
+        titles = [element.text for element in title_elements]
+
+        return titles
+
+    def get_tags(self, driver):
+        tags_container = driver.find_elements(By.CSS_SELECTOR, self.config.tag_selector)
+
+        tags = [tag.text for tag in tags_container]
+
+        return tags
 
     def download_tracks(self, links):
         k = 0

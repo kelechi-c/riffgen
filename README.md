b8ac946ef5a38935ecd1549409d5bec5b946384e
Tensor <narutokelechi@gmail.com>
2024-08-07 10:04:55 +0100

    [modify] add dataset info to readme, trim configs

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ A tiny generative model for music.
 
 #### architecture
 currently experimenting DiTs(Stabiltiy AI's Stable Audio). Partial/downsized implementation of the paper, 'Long-form music generation with latent diffusion'.
+
+#### dataset
+I personally scraped royalty-free music tracks from <a href="https://freesound.org">Freesound</a>. The code for scraping is included in the repo. The dataset is also available on 
+<a href="https://huggingface.co/datasets/tensorkelechi/vivy_audio">HuggingFace</a>
--- music_data/config.py
+++ music_data/config.py
@@ -1,14 +1,9 @@
 class scraper_config:
     sdd_dataset_url = "https://zenodo.org/records/10072001/files/audio.zip?download=1"
-    num_files_target = 1000
-    outpath = "music"
-    num_pages = 10
+    outpath = "data"
+    num_pages = 500
 
     # freesound scraping configs
-    freesound_url = "https://freesound.org/"
-    freesound_url_for_music = (
-        "https://freesound.org/browse/tags/?f=tag%3A%22music%22&page=1#sound"
-    )
     player_id = "div.bw-player"
     mp3_attr = "data-mp3"
     duration_attr = "data-duration"
@@ -17,4 +12,3 @@ class scraper_config:
     title_selector = "h5.v-spacing-1 > a.bw-link--black"
     tag_selector = "a.tag-container"
     tag_container = "div.bw-player__tags-list-height"
-    pagination_links = "bw-pagination_container"
